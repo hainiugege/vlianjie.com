2f8f429be37b40508d445a28142a8207361397cf
hainiugege <75881931+hainiugege@users.noreply.github.com>
2025-03-27 20:30:53 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -1,14 +1,9 @@
 // src/App.tsx
 
 import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
-  const [name, setName] = useState("unknown");
-
   return (
     <>
         <p>
