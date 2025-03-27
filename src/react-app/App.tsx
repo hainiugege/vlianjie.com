f59186a22f538b8d34bdda6b947fbf9fce74abdf
hainiugege <75881931+hainiugege@users.noreply.github.com>
2025-03-27 20:52:20 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -18,13 +18,13 @@ function App() {
   <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark py-4 shadow">
     <div class="container">
       <a class="navbar-brand text-decoration-none" href="#">链接跨境</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarCollapse">
         <ul class="navbar-nav me-auto mb-2 mb-md-0">
           <li class="nav-item">
-            <a class="nav-link active" aria-current="page" href="/#">首页</a>
+            <a class="nav-link active" href="/#">首页</a>
           </li>
           <li class="nav-item">
             <a class="nav-link" href="/#company">关于我们</a>
