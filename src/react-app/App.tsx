98a9023d05f1050edb29c2d0c99af3d76b1445d2
hainiugege <75881931+hainiugege@users.noreply.github.com>
2025-03-27 20:44:32 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -4,9 +4,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import "./bootstrap.min.css";
-import "./carousel.css";
-import "./bootstrap.bundle.min.js";
+
 
 function App() {
   const [count, setCount] = useState(0);
