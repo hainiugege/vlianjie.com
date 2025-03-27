9b12f7a71f7aa0c2b6964986f750d8b2670441b6
hainiugege <75881931+hainiugege@users.noreply.github.com>
2025-03-27 20:49:34 +0800

    Update App.tsx

--- src/react-app/App.tsx
+++ src/react-app/App.tsx
@@ -14,6 +14,35 @@ function App() {
 
   return (
     <>
+<header>
+  <nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark py-4 shadow">
+    <div class="container">
+      <a class="navbar-brand text-decoration-none" href="#">链接跨境</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav me-auto mb-2 mb-md-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#">首页</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#company">关于我们</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#culture">企业文化</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">联系我们</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#contact">成为供货商</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+</header>
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
